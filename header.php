49cae111e482a55407aadf0f8702ccaea625d40b
Andry Pedak <andry.pedak@gmail.com>
2017-08-13 14:53:16 +0300

    Added header

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Salong Iris</title>
+
+  <!-- Description (150 characters) -->
+  <meta name="description" content="A description of the page">
+
+  <!-- Base URL -->
+  <base href="http://iris.dev/index.php">
+
+  <!-- Meta -->
+  <meta name="robots" content="index,follow">
+  <meta name="googlebot" content="index,follow">
+  <meta name="google" content="notranslate">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php include 'header.php'; ?>
 
-  <title>Salong Iris</title>
-
-  <!-- Description (150 characters) -->
-  <meta name="description" content="A description of the page">
-
-  <!-- Base URL -->
-  <base href="http://iris.dev/index.php">
-
-  <!-- Meta -->
-  <meta name="robots" content="index,follow">
-  <meta name="googlebot" content="index,follow">
-  <meta name="google" content="notranslate">
-
-  <!-- Stylesheets -->
-  <link rel="stylesheet" href="main.css">
-</head>
-<body>
   
 </body>
 </html>
