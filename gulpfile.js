f699d127a97e2e9f302588eb497cdbc979675f8b
Andry Pedak <andry.pedak@gmail.com>
2017-08-13 14:45:11 +0300

    Notify configuration

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var notify = require("gulp-notify");
 gulp.task('sass', function () {
   return gulp.src('./src/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(notify("SASS Compiled! (<%= file.relative %>)"))
+    .pipe(notify({title: "SASS", message: "SASS Compiled! (<%= file.relative %>)", timeout: 2}))
     .pipe(gulp.dest('./src/css'));
 });
 
