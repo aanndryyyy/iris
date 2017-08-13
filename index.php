53b21d0f936158e66b1089ab19d71b6587ae9cb4
Andry Pedak <andry.pedak@gmail.com>
2017-08-13 14:15:43 +0300

    Base file structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Salong Iris</title>
+
+  <!-- Base URL -->
+  <base href="http://iris.dev/index.php">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  
+</body>
+</html>
