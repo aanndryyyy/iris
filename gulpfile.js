75c3e7aa4d851d27c2b832ee9f6278ebbdd5afc5
Andry Pedak <andry.pedak@gmail.com>
2017-08-13 14:39:15 +0300

    Added notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require("gulp-notify");
 
 gulp.task('sass', function () {
   return gulp.src('./src/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(notify("SASS Compiled! (<%= file.relative %>)"))
     .pipe(gulp.dest('./src/css'));
 });
 
 gulp.task('default', function() {
-  
+
   gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
