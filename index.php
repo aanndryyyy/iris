631a56e61feb414d3187297d11609cd7664c42e4
Andry Pedak <andry.pedak@gmail.com>
2017-08-13 14:21:53 +0300

    Added meta

--- index.php
+++ index.php
@@ -7,8 +7,16 @@
 
   <title>Salong Iris</title>
 
+  <!-- Description (150 characters) -->
+  <meta name="description" content="A description of the page">
+
   <!-- Base URL -->
   <base href="http://iris.dev/index.php">
+
+  <!-- Meta -->
+  <meta name="robots" content="index,follow">
+  <meta name="googlebot" content="index,follow">
+  <meta name="google" content="notranslate">
 
   <!-- Stylesheets -->
   <link rel="stylesheet" href="main.css">
